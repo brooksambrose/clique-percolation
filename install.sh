6dc86d8e7810e8f4b1ec7946b14f9b6fe99bb8b2
Brooks Ambrose <brooksambrose@gmail.com>
2015-09-23 22:24:08 -0700

    EC2 test
    seeing if install of cosparallel will work from EC2 instance

--- install.sh
+++ install.sh
@@ -19,9 +19,10 @@ cd ~/clique-percolation
 mkdir out49utel
 zcat in49/bel2mel-49utel.txt.gz | cut -f1-2 > out49utel/49utel.twocol.txt
 cd out49utel
-~/cosparallel-code/extras/maximal_cliques 
+~/cosparallel-code/extras/maximal_cliques 49utel.twocol.txt
+~cosparallel-code/cos -P 1 49utel.twocol.txt.mcliques
 
-zcat in49/bel2mel-49utel.txt.gz | cut -f1-2 | ~/cosparallel-code/extras/maximal_cliques
+#zcat in49/bel2mel-49utel.txt.gz | cut -f1-2 | ~/cosparallel-code/extras/maximal_cliques
 
 #TODO maximal cliques expects unweighted edgelist; how to load from .gz and exclude third column (weights)
 #TODO how to manage input and output for each data source
@@ -34,18 +35,18 @@ zcat in49/bel2mel-49utel.txt.gz | cut -f1-2 | ~/cosparallel-code/extras/maximal_
 
 
 #git command line usage
-oski@BCE:~/clique-percolation$ git add install.sh 
-oski@BCE:~/clique-percolation$ git commit -m 'instructions for using cut on data'
-[master 09c5986] instructions for using cut on data
- 1 file changed, 3 insertions(+)
-oski@BCE:~/clique-percolation$ git push
-Username for 'https://github.com': brooksambrose
-Password for 'https://brooksambrose@github.com': 
+#oski@BCE:~/clique-percolation$ git add install.sh 
+#oski@BCE:~/clique-percolation$ git commit -m 'instructions for using cut on data'
+#[master 09c5986] instructions for using cut on data
+# 1 file changed, 3 insertions(+)
+#oski@BCE:~/clique-percolation$ git push
+#Username for 'https://github.com': brooksambrose
+#Password for 'https://brooksambrose@github.com': 
 
-Counting objects: 3, done.
-Compressing objects: 100% (3/3), done.
-Writing objects: 100% (3/3), 388 bytes | 0 bytes/s, done.
-Total 3 (delta 2), reused 0 (delta 0)
-To https://www.github.com/brooksambrose/clique-percolation
-   f6d3ea8..09c5986  master -> master
+#Counting objects: 3, done.
+#Compressing objects: 100% (3/3), done.
+#Writing objects: 100% (3/3), 388 bytes | 0 bytes/s, done.
+#Total 3 (delta 2), reused 0 (delta 0)
+#To https://www.github.com/brooksambrose/clique-percolation
+#   f6d3ea8..09c5986  master -> master
 
