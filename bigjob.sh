5cdbc31b69e0fa73ae67eddf3996b5b3e5c9e5d7
Brooks Ambrose <brooksambrose@gmail.com>
2015-09-24 21:20:08 -0700

    reduce cos memory allocation

--- bigjob.sh
+++ bigjob.sh
@@ -7,7 +7,7 @@ mkdir out99utel
 time zcat in99/bel2mel-99utel.txt.gz | cut -f1-2 > out99utel/99utel.twocol.txt
 cd out99utel
 time ~/cosparallel-code/extras/maximal_cliques 99utel.twocol.txt
-time ~/cosparallel-code/cos -P 1 -W 29.9 99utel.twocol.txt.mcliques
+time ~/cosparallel-code/cos -P 1 -W 29.8 99utel.twocol.txt.mcliques
 
 ### Full analysis on 1900-1999 data as larger CR-UT-CR network
 cd ~/clique-percolation-data
@@ -15,7 +15,7 @@ mkdir out99crel
 time zcat in99/bel2mel-99crel.txt.gz | cut -f1-2 > out99crel/99crel.twocol.txt
 cd out99crel
 time ~/cosparallel-code/extras/maximal_cliques 99crel.twocol.txt
-time ~/cosparallel-code/cos -P 1 -W 29.9 99crel.twocol.txt.mcliques
+time ~/cosparallel-code/cos -P 1 -W 29.8 99crel.twocol.txt.mcliques
 
 ### Full analysis on 1900-1999 data as smaller resolved identity UT-CR-UT network
 cd ~/clique-percolation-data
@@ -23,7 +23,7 @@ mkdir out99zutel
 time zcat in99/bel2mel-99zutel.txt.gz | cut -f1-2 > out99zutel/99zutel.twocol.txt
 cd out99zutel
 time ~/cosparallel-code/extras/maximal_cliques 99zutel.twocol.txt
-time ~/cosparallel-code/cos -P 1 -W 29.9 99zutel.twocol.txt.mcliques
+time ~/cosparallel-code/cos -P 1 -W 29.8 99zutel.twocol.txt.mcliques
 
 ### Full analysis on 1900-1999 data as larger resolved identity CR-UT-CR network
 cd ~/clique-percolation-data
@@ -31,6 +31,6 @@ mkdir out99zcrel
 time zcat in99/bel2mel-99zcrel.txt.gz | cut -f1-2 > out99zcrel/99zcrel.twocol.txt
 cd out99zcrel
 time ~/cosparallel-code/extras/maximal_cliques 99zcrel.twocol.txt
-time ~/cosparallel-code/cos -P 1 -W 29.9 99zcrel.twocol.txt.mcliques
+time ~/cosparallel-code/cos -P 1 -W 29.8 99zcrel.twocol.txt.mcliques
 
 ### Total time:
